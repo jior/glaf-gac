cdfc684c39f8b2e73e236d72d55524de34d4cb7a
isdp <jior2008@gmail.com>
2013-08-11 12:14:15 +0800

    update

--- glaf-base/src/main/java/com/glaf/base/modules/sys/service/SysDepartmentServiceImpl.java
+++ glaf-base/src/main/java/com/glaf/base/modules/sys/service/SysDepartmentServiceImpl.java
@@ -10,6 +10,7 @@ import com.glaf.base.dao.AbstractSpringDao;
 import com.glaf.base.modules.sys.SysConstants;
 import com.glaf.base.modules.sys.model.SysDepartment;
 import com.glaf.base.modules.sys.model.SysTree;
+import com.glaf.base.utils.Authentication;
 import com.glaf.base.utils.PageResult;
 
 public class SysDepartmentServiceImpl implements SysDepartmentService {
@@ -125,7 +126,21 @@ public class SysDepartmentServiceImpl implements SysDepartmentService {
 	 * @return boolean
 	 */
 	public boolean delete(SysDepartment bean) {
-		return sysTreeService.delete(bean.getId());
+		if (Authentication.getAuthenticatedUser() != null) {
+			if (Authentication.getAuthenticatedUser().isSystemAdmin()
+					|| Authentication.getAuthenticatedUser()
+							.isDepartmentAdmin()) {
+				if (bean.getNode() != null) {
+					sysTreeService.delete(bean.getNode().getId());
+				} else {
+					if (bean.getNodeId() != 0) {
+						sysTreeService.delete(bean.getNodeId());
+					}
+				}
+				return abstractDao.delete(bean);
+			}
+		}
+		return false;
 	}
 
 	/**
